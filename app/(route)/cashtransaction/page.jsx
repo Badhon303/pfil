cd099e49fb95e4142005d76e43205f805121d4ee
BadhonDev <badhon@ebiw.com>
2024-01-02 18:15:31 +0600

    vault enabled

--- app/(route)/cashtransaction/page.jsx
+++ app/(route)/cashtransaction/page.jsx
@@ -1,12 +1,17 @@
+import { redirect } from "next/navigation"
+
 const Cashtransaction = () => {
-  return (
-    <div className="full">
-      <iframe
-        className="frame"
-        src="https://app.powerbi.com/reportEmbed?reportId=152a650f-ae45-4366-8ae5-4b3b767c1498&autoAuth=true&ctid=147d207c-cfb1-4dda-bebc-d96c8cb48eed"
-      ></iframe>
-    </div>
+  return redirect(
+    "https://app.powerbi.com/reportEmbed?reportId=152a650f-ae45-4366-8ae5-4b3b767c1498&autoAuth=true&ctid=147d207c-cfb1-4dda-bebc-d96c8cb48eed"
   )
+  // return (
+  //   <div className="full">
+  //     <iframe
+  //       className="frame"
+  //       src="https://app.powerbi.com/reportEmbed?reportId=152a650f-ae45-4366-8ae5-4b3b767c1498&autoAuth=true&ctid=147d207c-cfb1-4dda-bebc-d96c8cb48eed"
+  //     ></iframe>
+  //   </div>
+  // )
 }
 
 export default Cashtransaction
