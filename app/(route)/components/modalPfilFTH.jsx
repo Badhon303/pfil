d03c5b6d058394b8357a6d46dc16408569ae7fac
BadhonDev <badhon@ebiw.com>
2024-09-24 10:18:41 +0600

    fixed investment

--- app/(route)/components/modalPfilFTH.jsx
+++ app/(route)/components/modalPfilFTH.jsx
@@ -88,7 +88,7 @@ const ModalPfilFTH = ({ token }) => {
                   fontSize: "20px",
                 }}
               >
-                Deposit & investment
+                Deposit & Investment
               </p>
             </DialogTrigger>
           )}
